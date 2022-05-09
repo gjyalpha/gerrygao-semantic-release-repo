689e2c8b0ad913f2e955f7cf145a8ab919e1af12
Gerry Gao <gjyalpha@gmail.com>
2022-05-09 00:16:56 -0700

    fix(test): test a big bug fix
    BREAKING CHANGE: It will break nothing. But just testing it.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export const sum = (a: number, b: number) => {
   if ('development' === process.env.NODE_ENV) {
-    console.log('boop');
+    console.log('boop. Now I am a new major version');
   }
   return a + b;
 };
